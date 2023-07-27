67e0bfe0db023ace336b114a5eafd409e1099f81
Kiran Sharma <kiran.sharma@gurzu.com>
2023-07-27 11:21:14 +0545

    Configured search button

--- lib/screen/teacher_phone_screen.dart
+++ lib/screen/teacher_phone_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:school_app/data/dataproviders/fetch_employee_data.dart';
 import 'package:school_app/data/models/employee.dart';
 import 'package:school_app/widgets/employee_list_widget.dart';
+import 'package:school_app/widgets/search_widgets.dart';
 
 class TeacherPhoneScreen extends StatefulWidget {
   const TeacherPhoneScreen({super.key});
@@ -11,40 +12,128 @@ class TeacherPhoneScreen extends StatefulWidget {
 }
 
 class _TeacherPhoneScreenState extends State<TeacherPhoneScreen> {
+  TextEditingController? _searchController;
+  String query = '';
+  late List<Employee> employee;
+  final _searchFocusNode = FocusNode();
+
   late List<Employee> employeeData;
 
   @override
-  void initState()  {
+  void initState() {
     employeeData = FetchDataApi.employeeList;
+    _searchController = TextEditingController(text: '');
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       body: SizedBox(
         height: double.infinity,
         width: double.infinity,
-        child: Stack(children: [
-          FutureBuilder(
-          future: FetchDataApi.loadAddress(),
-          builder: (context, AsyncSnapshot<List<Employee>> snapshot) {
-            if (snapshot.hasError) {
-              return const Center(
-                child: Text(
-                  "Error fetching data",
-                ),
-              );
-            }
-            if (!snapshot.hasData) {
-              return const Center(child: CircularProgressIndicator());
-            }
-            return dashboardBody(context, employeeData);
-          }), 
-        ]),
+        child: Stack(
+          children: [
+            employeeDashboardBody(),
+            employeeBar(context),
+            searchWidget(context)
+          ],
+        ),
       ),
     );
+  }
+
+  Container employeeBar(BuildContext context) {
+    return Container(
+      height: 129,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Colors.blue,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(
+          top: 10,
+          left: 16,
+          right: 16,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Text(
+              "Teachers",
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  FutureBuilder<List<Employee>> employeeDashboardBody() {
+    return FutureBuilder(
+        future: FetchDataApi.loadAddress(),
+        builder: (context, AsyncSnapshot<List<Employee>> snapshot) {
+          if (snapshot.hasError) {
+            return const Center(
+              child: Text(
+                "Error fetching data",
+              ),
+            );
+          }
+          if (!snapshot.hasData) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          return Padding(
+            padding: const EdgeInsets.only(top: 128.0),
+            child: dashboardBody(context, employeeData),
+          );
+        });
+  }
+
+  SizedBox searchWidget(BuildContext context) {
+    return SizedBox(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 16,
+        ),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 101,
+            ),
+            SizedBox(
+              height: 49,
+              child: Material(
+                elevation: 2,
+                color: Colors.white,
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(16),
+                ),
+                child: SearchWidget(
+                  onChanged: searchItems,
+                  text: query,
+                  isFocus: false,
+                  controller: _searchController,
+                  focusNode: _searchFocusNode,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  void searchItems(String query) {
+    final items = FetchDataApi.employeeList.where((item) {
+      final titleLower = item.name!.toLowerCase();
+      final searchLower = query.toLowerCase();
+
+      return titleLower.contains(searchLower);
+    }).toList();
+    setState(() {
+      this.query = query;
+      employeeData = items;
+    });
   }
 }
 
@@ -55,6 +144,7 @@ Widget dashboardBody(BuildContext context, List<Employee> employeeData) {
     child: ListView.builder(
         itemCount: employeeData.length,
         itemBuilder: (c, i) {
+          employeeData.sort((a, b) => a.name!.compareTo(b.name!));
           return EmployeeListWidget(
             name: employeeData[i].name,
             phoneNo: employeeData[i].phone,
---
+++ lib/widgets/search_widgets.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class SearchWidget extends StatelessWidget {
+  final TextEditingController? controller;
+  final ValueChanged<String> onChanged;
+  final String? text;
+  final bool? isFocus;
+  final FocusNode? focusNode;
+
+  const SearchWidget({super.key, this.controller, required this.onChanged, this.text, this.isFocus, this.focusNode});
+
+   @override
+  Widget build(BuildContext context) {
+    return TextField(
+      cursorColor: Theme.of(context).primaryColor,
+      controller: controller,
+      focusNode: focusNode,
+      autofocus: isFocus!,
+      decoration: InputDecoration(
+        hintStyle: TextStyle(
+          color: Colors.grey.shade100,
+        ),
+        hintText:"Search",
+        prefixIcon: const Icon(
+          Icons.search,
+        ),
+        suffixIcon: text!.isNotEmpty
+            ? GestureDetector(
+                child: const Icon(Icons.close),
+                onTap: () {
+                  controller!.clear();
+                  onChanged('');
+                  FocusScope.of(context).requestFocus(FocusNode());
+                },
+              )
+            : null,
+        border: InputBorder.none,
+        contentPadding: const EdgeInsets.symmetric(
+          horizontal: 24,
+          vertical: 12,
+        ),
+      ),
+      onChanged: onChanged,
+    );
+  }
+}
